77a14a05dca40afcdb26b9c0a802e423a76ee92e
unknown <27kevin.bech@gmail.com>
2024-10-29 20:36:15 +0700

    proc

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,6 @@
 #include <GLFW/glfw3.h>
 
 
-    
-    void framebuffer_size_callback(GLFWwindow* window, int width, int height){
-        glViewport(0, 0, width, height);
-        std::cout << "resizing";
-        glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(window); 
-    }  
 
 
 main(){
@@ -31,24 +24,5 @@ main(){
         std::cout << "Error GLAD";
     }
 
-    glViewport(0,0,800,600);
-
-
-
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-
-    
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
-
-    while(!glfwWindowShouldClose(window)){
-
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwPollEvents();
-        glfwSwapBuffers(window);
-    }
-
-
-    glfwTerminate();
     return 0;
 }
