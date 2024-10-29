3a11505733b6d2d083fd215a901744c438ae8145
unknown <27kevin.bech@gmail.com>
2024-10-29 20:50:07 +0700

    new-logic

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,41 @@
 #include <GLFW/glfw3.h>
 
 
-
-
-main(){
-
+void init(){
     glfwInit();
     //triple hints
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
 
-  
-
-
+void windowLoad(){
     GLFWwindow* window = glfwCreateWindow(800, 600, "Hello world", NULL, NULL);
     glfwMakeContextCurrent(window);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();    
+    }
+} 
+
+main(){
+
+    init();
+
+    GLFWwindow* window;
+
+    windowLoad();
     
     
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
         std::cout << "Error GLAD";
     }
+
+        
+
+
 
     return 0;
 }
