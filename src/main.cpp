dee0b233036e6014e1fe649ba60ec6d084690c03
unknown <27kevin.bech@gmail.com>
2024-11-01 15:16:32 +0700

    add some shader to compile

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,7 @@ void frameDynamic(GLFWwindow* window, int width, int height){
 main(){
 
     const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n
+"layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
